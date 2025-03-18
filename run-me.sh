93d2d0158614ad6b697a76bfdacc1fc984ee2494
vhoeksma <hoeksmav@yahoo.nl>
2025-03-18 12:37:17 +0100

    Update run-me.sh

--- run-me.sh
+++ run-me.sh
@@ -1,92 +1,117 @@
 #!/bin/bash -v
 
-MARIAN=../../build
+MARIAN=../..
 
-# if we are in WSL, we need to add '.exe' to the tool names
-if [ -e "/bin/wslpath" ]
-then
-    EXT=.exe
-fi
+# Set source and target languages
+SRC=nl  # Dutch
+TGT=de  # German
 
-MARIAN_TRAIN=$MARIAN/marian$EXT
-MARIAN_DECODER=$MARIAN/marian-decoder$EXT
-MARIAN_VOCAB=$MARIAN/marian-vocab$EXT
-MARIAN_SCORER=$MARIAN/marian-scorer$EXT
-
-# set chosen gpus
+# Set chosen GPUs
 GPUS=0
-if [ $# -ne 0 ]
-then
+if [ $# -ne 0 ]; then
     GPUS=$@
 fi
-echo Using GPUs: $GPUS
+echo "Using GPUs: $GPUS"
 
-if [ ! -e $MARIAN_TRAIN ]
-then
-    echo "marian is not installed in $MARIAN, you need to compile the toolkit first"
+if [ ! -e $MARIAN/build/marian ]; then
+    echo "Marian is not installed in $MARIAN/build, you need to compile the toolkit first"
     exit 1
 fi
 
-if [ ! -e ../tools/moses-scripts ] || [ ! -e ../tools/subword-nmt ]
-then
-    echo "missing tools in ../tools, you need to download them first"
-    exit 1
-fi
+# List of download links for training data
+DATA_URLS=(
+    "https://object.pouta.csc.fi/OPUS-CCMatrix/v1/moses/de-nl.txt.zip CCMatrix-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-NLLB/v1/moses/de-nl.txt.zip NLLB-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-OpenSubtitles/v2024/moses/de-nl.txt.zip OpenSubtitles-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-MultiParaCrawl/v9b/moses/de-nl.txt.zip MultiParaCrawl-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-MultiCCAligned/v1.1/moses/de-nl.txt.zip MultiCCAligned-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-ELRC-EMEA/v1/moses/de-nl.txt.zip ELRC-EMEA-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-EUbookshop/v2/moses/de-nl.txt.zip EUbookshop-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-DGT/v2019/moses/de-nl.txt.zip DGT-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-TildeMODEL/v2018/moses/de-nl.txt.zip TildeMODEL-de-nl.txt.zip"
+    "https://object.pouta.csc.fi/OPUS-LinguaTools-WikiTitles/v2014/moses/de-nl.txt.zip LinguaTools-WikiTitles-de-nl.txt.zip"
+)
 
-if [ ! -e "data/corpus.nl" ]
-then
-    ./scripts/download-files-dutch.sh
-fi
+# Create data directory if it does not exist
+mkdir -p data
+cd data
 
+# Download training data
+for ENTRY in "${DATA_URLS[@]}"; do
+    URL=$(echo $ENTRY | awk '{print $1}')
+    FILE_NAME=$(echo $ENTRY | awk '{print $2}')
+    
+    if [ ! -e "$FILE_NAME" ]; then
+        echo "Downloading $FILE_NAME..."
+        wget -nc "$URL" -O "$FILE_NAME"
+    else
+        echo "$FILE_NAME already exists, skipping download."
+    fi
+done
+
+# Extract downloaded data
+for FILE in *.zip; do
+    echo "Extracting $FILE..."
+    unzip -o "$FILE"
+done
+
+# Concatenate corpus files
+cat *.$SRC > corpus.$SRC
+cat *.$TGT > corpus.$TGT
+
+# Clean up extracted individual files
+rm -f *.zip
+
+# Change back to main directory
+cd ..
+
+# Get our fork of sacreBLEU
+git clone https://github.com/marian-nmt/sacreBLEU.git sacreBLEU
+
+# Manually set up dev and test sets from the corpus data
+echo "Manually setting up validation and test sets..."
+
+# Example: Use the first 1000 lines of the corpus as the validation set
+head -n 1000 data/corpus.$SRC > data/newsdev2016.$SRC
+head -n 1000 data/corpus.$TGT > data/newsdev2016.$TGT
+
+# Example: Use the next 1000 lines as the test set
+tail -n +1001 data/corpus.$SRC | head -n 1000 > data/newstest2016.$SRC
+tail -n +1001 data/corpus.$TGT | head -n 1000 > data/newstest2016.$TGT
+
+# Create the model folder
 mkdir -p model
 
-# preprocess data
-if [ ! -e "data/corpus.bpe.nl" ]
-then
-    ./scripts/preprocess-data.sh
-fi
+# Train the model
+$MARIAN/build/marian \
+    --devices $GPUS \
+    --type s2s \
+    --model model/model.npz \
+    --train-sets data/corpus.$SRC data/corpus.$TGT \
+    --vocabs model/vocab.$SRC$TGT.spm model/vocab.$SRC$TGT.spm \
+    --dim-vocabs 32000 32000 \
+    --mini-batch-fit -w 5000 \
+    --layer-normalization --tied-embeddings-all \
+    --dropout-rnn 0.2 --dropout-src 0.1 --dropout-trg 0.1 \
+    --early-stopping 5 --max-length 100 \
+    --valid-freq 10000 --save-freq 10000 --disp-freq 1000 \
+    --cost-type ce-mean-words --valid-metrics ce-mean-words bleu-detok \
+    --valid-sets data/newsdev2016.$SRC data/newsdev2016.$TGT \
+    --log model/train.log --valid-log model/valid.log --tempdir model \
+    --overwrite --keep-best \
+    --seed 1111 --exponential-smoothing \
+    --normalize=0.6 --beam-size=6 --quiet-translation
 
-# train model
-if [ ! -e "model/model.npz.best-translation.npz" ]
-then
-    $MARIAN_TRAIN \
-        --devices $GPUS \
-        --type amun \
-        --model model/model.npz \
-        --train-sets data/corpus.bpe.nl data/corpus.bpe.de \
-        --vocabs model/vocab.nl.yml model/vocab.de.yml \
-        --dim-vocabs 100000 100000 \
-        --mini-batch-fit -w 15000 \
-        --layer-normalization --dropout-rnn 0.2 --dropout-src 0.1 --dropout-trg 0.1 \
-        --early-stopping 5 \
-        --valid-freq 10000 --save-freq 10000 --disp-freq 1000 \
-        --valid-metrics cross-entropy translation \
-        --valid-sets data/corpus-dev.bpe.nl data/corpus-dev.bpe.de \
-        --valid-script-path "bash ./scripts/validate.sh" \
-        --log model/train.log --valid-log model/valid.log \
-        --overwrite --keep-best \
-        --seed 1111 --exponential-smoothing \
-        --normalize=1 --beam-size=12 --quiet-translation
-fi
+# Translate dev set
+cat data/newsdev2016.$SRC \
+    | $MARIAN/build/marian-decoder -c model/model.npz.best-bleu-detok.npz.decoder.yml -d $GPUS -b 6 -n0.6 \
+      --mini-batch 64 --maxi-batch 100 --maxi-batch-sort src > data/newsdev2016.$SRC.output
 
-# translate dev set
-cat data/corpus-dev.bpe.nl \
-    | $MARIAN_DECODER -c model/model.npz.best-translation.npz.decoder.yml -d $GPUS -b 12 -n1 \
-      --mini-batch 64 --maxi-batch 10 --maxi-batch-sort src \
-    | sed 's/\@\@ //g' \
-    | ../tools/moses-scripts/scripts/recaser/detruecase.perl \
-    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l de \
-    > data/corpus-dev.nl.output
+# Translate test set
+cat data/newstest2016.$SRC \
+    | $MARIAN/build/marian-decoder -c model/model.npz.best-bleu-detok.npz.decoder.yml -d $GPUS -b 6 -n0.6 \
+      --mini-batch 64 --maxi-batch 100 --maxi-batch-sort src > data/newstest2016.$SRC.output
 
-# translate test set
-cat data/corpus-test.bpe.nl \
-    | $MARIAN_DECODER -c model/model.npz.best-translation.npz.decoder.yml -d $GPUS -b 12 -n1 \
-      --mini-batch 64 --maxi-batch 10 --maxi-batch-sort src \
-    | sed 's/\@\@ //g' \
-    | ../tools/moses-scripts/scripts/recaser/detruecase.perl \
-    | ../tools/moses-scripts/scripts/tokenizer/detokenizer.perl -l de \
-    > data/corpus-test.nl.output
-
-# calculate bleu scores on dev and test set
-../tools/moses-scripts/scripts/generic/multi-bleu-detok.perl data/corpus-dev.de < data/corpus-dev.nl.output
-../tools/moses-scripts/scripts/generic/multi-bleu-detok.perl data/corpus-test.de < data/corpus-test.nl.output
+# Calculate BLEU scores
+sacreBLEU/sacrebleu.py -t wmt16/dev -l $SRC-$TGT < data/newsdev2016.$SRC.output
+sacreBLEU/sacrebleu.py -t wmt16 -l $SRC-$TGT < data/newstest2016.$SRC.output
